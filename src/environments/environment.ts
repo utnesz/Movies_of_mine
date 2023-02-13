114fcc54835fe6b95be9223228742b3a5145e905
utnesz <90829509+utnesz@users.noreply.github.com>
2023-02-13 11:00:12 +0100

    Ini_com

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: 'https://nettuts.hu/jms/utnesz/cinema',
+  production: false,
+  apiUrl: 'https://nettuts.hu/jms/utnes/cinema',
 };
 
 /*
